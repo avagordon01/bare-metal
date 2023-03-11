e671fa8787972838a256fb5b09dafc2d55b62816
Ava Gordon <avagordon01@gmail.com>
2023-03-11 01:26:21 +0000

    simpler build steps, instead using qemu fat emulation

--- uefi/test.sh
+++ uefi/test.sh
@@ -7,15 +7,9 @@ if [ ! -d out ]; then
 fi
 meson install -C out
 
-dd if=/dev/zero of=image bs=1k count=1440
-mformat -i image -f 1440 ::
-mmd -i image ::/EFI
-mmd -i image ::/EFI/BOOT
-mcopy -i image packaged/bin/main ::/EFI/BOOT/BOOTX64.EFI
-
+mkdir -p disk/EFI/BOOT
+cp packaged/bin/main disk/EFI/BOOT/BOOTX64.EFI
 qemu-system-x86_64 \
     -enable-kvm -cpu host \
     -bios /usr/share/edk2/x64/OVMF.fd \
-    -drive if=none,id=stick,format=raw,file=./image \
-    -device nec-usb-xhci,id=xhci \
-    -device usb-storage,bus=xhci.0,drive=stick
+    -drive format=raw,file=fat:rw:disk
